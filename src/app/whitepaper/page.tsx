0d8fd0f71cf5ecaaa9647cd090028689999f324e
“brunolune” <brunocoupier@protonmail.com>
2023-05-09 21:41:18 +0200

    added content and schematic to whitepaper

--- src/app/whitepaper/page.tsx
+++ src/app/whitepaper/page.tsx
@@ -8,11 +8,10 @@ import Image from "next/image";
 
 export default function Whitepaper() {
   return (
-    <main className="mx-5 my-5">
-      <div>
-        {" "}
-        <main className="max-w-xl">
-          <h1 className="mt-5 text-2xl p-5">DBrains Whitepaper</h1>
+    <main className="">
+      <h1 className="mt-5 text-2xl p-5">DBrains Whitepaper</h1>
+      <div className="mx-5 my-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3">
+        <div>
           <p className="text-2xl p-5">Intro </p>
           <p className="text-lg p-5">
             DBrains (for Decentralized Brains or DAO Brains) is a fully
@@ -31,7 +30,8 @@ export default function Whitepaper() {
             federation of collective intelligence and community governance to
             large communities.
           </p>
-          <br />
+        </div>
+        <div>
           <p className="text-2xl p-5">Principles: </p>
           <ul className="text-2xl p-5">
             <li>
@@ -58,7 +58,34 @@ export default function Whitepaper() {
               </p>
             </li>{" "}
           </ul>
-          <p className="text-2xl p-5">How does it work? </p>{" "}
+        </div>
+        <div>
+          <p className="text-2xl p-5">Objectives </p>
+          <ul className="text-lg  p-5">
+            <li>
+              Federating / amplifying collective intelligence by improving the
+              efficiency of collaborative working.
+            </li>
+            <br />
+            {/* <li> Aligning individual incentives with the general interest</li>
+            <br /> */}
+            <li>
+              Creating a platform of fair collaborative working and learning.
+            </li>{" "}
+            <br />
+            <li>
+              Lowering the barrier of job accession, allowing anyone interested
+              to contribute.
+            </li>
+            <br />
+            <li>
+              Empowering communities to self-organize, fulfilling the purpose of
+              blockchain
+            </li>{" "}
+          </ul>
+        </div>{" "}
+        <div>
+          <p className="text-2xl p-5">How does it work?</p>
           <p className="text-lg p-5">
             Members can propose new projects which are listed in the projects
             board. There can be many sort of projects, like &quot;paid
@@ -80,13 +107,15 @@ export default function Whitepaper() {
             for the setup of bounties, or Sourcecred.io for the monitoring of
             contribution could be integrated into the platform for this purpose.
           </p>
-          <Image
-            className=" p-5 block  max-w-max"
-            src="/dbrains_functioning3.png"
-            width={1300}
-            height={1300}
-            alt="DBrains functioning"
-          />
+        </div>
+        <Image
+          className=" p-5 w-full object-cover"
+          src="/dbrains_functioning6.png"
+          width={450}
+          height={450}
+          alt="DBrains functioning"
+        />
+        <div>
           <p className="text-2xl p-5">Platform Tokenomics:</p>{" "}
           <p className="text-lg p-5">
             The DBR token associated with the main DAO&apos;s platform is a Non
@@ -113,7 +142,55 @@ export default function Whitepaper() {
             their project. Projects launchers will pay some fee to the DBrains
             DAO for using the Platform.
           </p>
-        </main>
+        </div>
+        <div>
+          {" "}
+          <p className="text-2xl p-5">Non Transferable Token (NTT)</p>
+          <ul className="p-5">
+            <li>
+              NTT stands for Non Transferable Token. They are burnable ERC20
+              token without transfer function. NTT is the generic name
+              (similarly to ERC20) for the token associated with the &quot;free
+              projects&quot; in DBrains.
+            </li>
+            <br />
+            <li>
+              At project launch, no NTTs have been emitted. New NTTs are minted
+              when project&apos;s DAO&apos;s members are rewarded for their
+              contribution to the project.
+            </li>
+            <br />
+
+            <li>
+              NTTs are similar to equities in a traditional company. Although
+              NTTs owners cannot transfer their NTTs. They can decide to burn
+              them in exchange of some currencies available in the DAO&quot;s
+              treasury when the project&apos;s DAO allows it.
+            </li>
+            <br />
+            <li>
+              NTTs of a member represent his/her contribution to the project,
+              and determine the fraction of the project&apos;s generated profit
+              (or fraction of payment if it is a paid project) corresponding to
+              his/her earned NTTs divided by the total NTTs in circulation.
+            </li>
+            <br />
+          </ul>
+        </div>
+        <Image
+          className=" p-5 w-full object-cover"
+          src="/dbrains_freeProject.png"
+          width={450}
+          height={450}
+          alt="DBrains free project"
+        />
+        <Image
+          className=" p-5 w-full object-cover"
+          src="/dbrains_paidProject.png"
+          width={450}
+          height={450}
+          alt="DBrains paid project"
+        />
       </div>
     </main>
   );
